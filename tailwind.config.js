b3021e593c4710a1c7abb1823cde31d985cc9cca
okang1014 <okang1014@gmail.com>
2024-12-23 21:09:59 +0900

    💄style: 회원가입 페이지 스타일 작업 1차 완료

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
       container: "1200px",
     },
     container: {
-      center: true, // Contanier 가운데 정렬
+      center: true, // Container 가운데 정렬
       padding: "100px", // Container 좌우 패딩 100px
     },
     colors: {
