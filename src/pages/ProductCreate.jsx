bd12b49951f1c16f21f1b64c641c1ad8440db429
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-27 15:52:00 +0900

    💄style: 상품 카테고리 option 글자 크기 변경 12px -> 14px

--- src/pages/ProductCreate.jsx
+++ src/pages/ProductCreate.jsx
@@ -61,8 +61,8 @@ export default function ProductCreate() {
           <div className="flex gap-[30px]">
             <select
               id="category-1"
-              className="w-[400px] h-[40px] border border-gray2 rounded-lg px-3 text-xs focus:outline-none"
-              name="selectedCategory"
+              className="w-[400px] h-[40px] border border-gray2 rounded-lg px-3 text-[14px] focus:outline-none"
+              name="selectedCategory1"
             >
               <option>상품 카테고리를 선택해주세요.</option>
               <option value="cartoon">만화/애니메이션</option>
@@ -74,7 +74,8 @@ export default function ProductCreate() {
             </select>
             <select
               id="category-2"
-              className="w-[400px] h-[40px] border border-gray2 rounded-lg px-3 text-xs focus:outline-none"
+              className="w-[400px] h-[40px] border border-gray2 rounded-lg px-3 text-[14px] focus:outline-none"
+              name="selectedCategory2"
             >
               <option>상품 카테고리를 선택해주세요.</option>
               <option value="cartoon">만화/애니메이션</option>
