ff84fbd1f909b8da2daa7ac25c991c40f0329efb
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-30 16:19:05 +0900

    💄style: 검색 결과 없음 페이지 마크업 완료

---
+++ src/components/SearchNoResult.jsx
@@ -0,0 +1,8 @@
+export default function SearchNoResult() {
+  return (
+    <div className="mt-[110px] flex flex-col items-center gap-[30px]">
+      <img src="/src/assets/images/error-char 1.png" alt="검색 결과 없음" />
+      <p className="text-[24px] font-bold">검색 결과가 없어요 😭</p>
+    </div>
+  );
+}
