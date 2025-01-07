e52865c2906c6544dbe1eba65f595b97dc1c4c9c
okang1014 <okang1014@gmail.com>
2025-01-07 20:49:15 +0900

    :sparkles:feat: 회원가입 페이지 기능 구현, InputField, password 컴포넌트 추가

--- src/pages/user/Signup.jsx
+++ src/pages/user/Signup.jsx
@@ -1,136 +1,127 @@
+import InputField from "@components/InputField";
+import PasswordInput from "@components/PasswordInput";
+import { useForm } from "react-hook-form";
+
+// 이메일, 비밀번호 정규 표현식
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const passwordRegex = /^.{8,}$/;
+
 export default function Signup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm();
+
+  const signup = () => {
+    console.log("회원가입 버튼 클릭");
+  };
+
   return (
     <div className="container">
-      <form className="w-[400px] mx-auto pb-[60px]">
+      <form
+        className="w-[400px] mx-auto pb-[60px]"
+        onSubmit={handleSubmit(signup)}
+      >
         <h1 className="text-center font-bold text-[32px] pb-[42px]">
           회원가입
         </h1>
 
         <fieldset className="mb-[10px]" id="userInfo">
           <legend className="sr-only">사용자 정보 입력</legend>
-          <div className="mb-5">
-            <label htmlFor="userName">이름</label>
-            <div className="py-[14px] border-solid border-b-4 border-gray3 focus-within:border-point-blue">
-              <input
-                className="text-[20px] w-full focus:outline-none"
-                id="userName"
-                type="text"
-                placeholder="예) 김아랜"
-              />
-            </div>
-            <p className="text-point-red mt-[2px]">
-              이름을 정확히 입력해주세요
-            </p>
-          </div>
+          <InputField
+            id="userName"
+            label="이름"
+            placeholder="예) 김아랜"
+            register={register("username", {
+              required: "이름을 입력해주세요.",
+              pattern: {
+                value: /^[가-힣]{2,5}$/,
+                message: "이름은 최대 5자까지 입력 가능합니다.",
+              },
+            })}
+            error={errors.username}
+          />
 
-          <div className="mb-5">
-            <label htmlFor="userNickname">닉네임</label>
-            <div className="py-[10px] flex border-solid border-b-4 border-gray3 focus-within:border-point-blue">
-              <input
-                className="text-[20px] focus:outline-none flex-grow"
-                id="userNickname"
-                type="text"
-                placeholder="예) 아이랜드덕후"
-              />
-              <div className="ml-auto px-[14px] py-[6px] border-solid border-2 border-gray2 rounded-lg text-gray3 box-content focus-within:border-point-blue">
-                <button
-                  type="button"
-                  className="cursor-pointer focus:outline-none"
-                  aria-label="닉네임 중복 확인 버튼"
-                >
-                  중복확인
-                </button>
-              </div>
+          <InputField
+            id="userNickname"
+            label="닉네임"
+            placeholder="예) 아이랜드덕후"
+            register={register("name", {
+              required: "닉네임을 입력해주세요.",
+              pattern: {
+                value: /^[가-힣a-zA-Z0-9]{2,16}$/,
+                message: "닉네임은 최대 16자까지 입력 가능합니다.",
+              },
+            })}
+            error={errors.name}
+          >
+            <div className="ml-auto px-[14px] py-[6px] border-solid border-2 border-gray2 rounded-lg text-gray3 box-content focus-within:border-point-blue">
+              <button
+                type="button"
+                className="cursor-pointer focus:outline-none"
+                aria-label="닉네임 중복 확인 버튼"
+              >
+                중복확인
+              </button>
             </div>
-            <p className="text-point-red mt-[2px]">
-              닉네임을 정확히 입력해주세요
-            </p>
-          </div>
+          </InputField>
 
-          <div className="mb-5">
-            <label htmlFor="userEmail">이메일</label>
-            <div className="py-[10px] flex border-solid border-b-4 border-gray3 focus-within:border-point-blue">
-              <input
-                className="text-[20px] focus:outline-none flex-grow"
-                id="userEmail"
-                type="email"
-                placeholder="예) iland@iland.com"
-              />
-              <div className="ml-auto px-[14px] py-[6px] border-solid border-2 border-gray2 rounded-lg text-gray3 box-content focus-within:border-point-blue">
-                <button
-                  type="button"
-                  className="cursor-pointer focus:outline-none"
-                  aria-label="이메일 중복 확인 버튼"
-                >
-                  중복확인
-                </button>
-              </div>
+          <InputField
+            id="email"
+            label="이메일"
+            placeholder="예) iland@iland.com"
+            register={register("email", {
+              required: "이메일을 입력해주세요.",
+              pattern: {
+                value: emailRegex,
+                message: "올바른 형식의 이메일을 입력해주세요.",
+              },
+            })}
+            error={errors.email}
+          >
+            <div className="ml-auto px-[14px] py-[6px] border-solid border-2 border-gray2 rounded-lg text-gray3 box-content focus-within:border-point-blue">
+              <button
+                type="button"
+                className="cursor-pointer focus:outline-none"
+                aria-label="닉네임 중복 확인 버튼"
+              >
+                중복확인
+              </button>
             </div>
-            <p className="text-point-red mt-[2px]">
-              이메일을 정확히 입력해주세요
-            </p>
-          </div>
+          </InputField>
         </fieldset>
 
         <fieldset id="userPw">
           <legend className="sr-only">비밀번호 입력 및 확인란</legend>
-          <div className="mb-5">
-            <label htmlFor="password">비밀번호</label>
-            <div className="py-[14px] flex border-solid border-b-4 border-gray3 focus-within:border-point-blue items-center">
-              <input
-                className="text-[20px] focus:outline-none flex-grow"
-                id="password"
-                type="password"
-                placeholder="비밀번호"
-              />
-              <button
-                type="button"
-                className="cursor-pointer"
-                aria-label="비밀번호 표시"
-              >
-                <img
-                  className="ml-auto"
-                  src="/assets/icons/eye-closed.svg"
-                  alt="비밀번호 숨김 상태"
-                />
-                <img
-                  className="hidden ml-auto"
-                  src="/assets/icons/eye-open.svg"
-                  alt="비밀번호 표시 중"
-                />
-              </button>
-            </div>
-            <p className="text-point-red mt-[2px]">비밀번호를 입력해주세요</p>
-          </div>
+          <PasswordInput
+            id="password"
+            label="비밀번호"
+            placeholder="비밀번호"
+            register={register("password", {
+              required: "비밀번호를 입력해주세요.",
+              pattern: {
+                value: passwordRegex,
+                message: "올바른 형식의 비밀번호를 입력해주세요.",
+              },
+            })}
+            error={errors.password}
+          />
 
-          <div className="mb-5">
-            <label htmlFor="passwordCheck">비밀번호 확인</label>
-            <div className="py-[14px] flex border-solid border-b-4 border-gray3 focus-within:border-point-blue items-center">
-              <input
-                className="text-[20px] focus:outline-none flex-grow"
-                id="passwordCheck"
-                type="password"
-                placeholder="비밀번호 확인"
-              />
-              <button
-                type="button"
-                className="cursor-pointer"
-                aria-label="비밀번호 확인 표시"
-              >
-                <img
-                  className="ml-auto"
-                  src="/assets/icons/eye-closed.svg"
-                  alt="비밀번호 숨김 상태"
-                />
-                <img
-                  className="hidden ml-auto"
-                  src="/assets/icons/eye-open.svg"
-                  alt="비밀번호 표시 중"
-                />
-              </button>
-            </div>
-            <p className="text-point-red mt-[2px]">비밀번호를 확인해주세요</p>
-          </div>
+          <PasswordInput
+            id="passwordCheck"
+            label="비밀번호 확인"
+            placeholder="비밀번호 확인"
+            register={register("passwordCheck", {
+              required: "비밀번호를 입력해주세요.",
+              pattern: {
+                value: passwordRegex,
+                message: "올바른 형식의 비밀번호를 입력해주세요.",
+              },
+            })}
+            error={errors.passwordCheck}
+          />
         </fieldset>
 
         <div className="text-center p-[18px] border-solid border-gray3 border-2 rounded-[8px] text-gray3 focus-within:border-point-blue focus-within:shadow-md focus-within:ring-point-blue">
