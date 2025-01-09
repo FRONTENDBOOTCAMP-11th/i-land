a1e35919424d2d75550bfd0288691fb2719b5f82
sunghyeon <ggjj7827@gmail.com>
2025-01-09 10:48:19 +0900

    ✨ feat: 리뷰 등록 조건을 충족하지 못했을 때 안내 메시지

--- src/components/layout/DetailFooter.jsx
+++ src/components/layout/DetailFooter.jsx
@@ -11,20 +11,9 @@ export default function DetailFooter({_id}) {
   const [loading, setLoading] = useState(true); // 로딩
   const [error, setError] = useState(null); // 에러
   const [product, setProduct] = useState(null); // 상품 정보
-  const [userInfo, setUserInfo] = useState(null); // 유저 정보
   const [productReview, setProductReview] = useState([]); // 상품 리뷰
   const [reviewContent, setReviewContent] = useState(""); // textarea 상태
 
-  // 현재 유저 정보 가져오기
-  const fetchUser = async () => {
-    if (!user?._id) return; // user가 없으면 조기 리턴
-    try {
-      const response = await axios.get(`/users/${user?._id}`); // user._id를 통해서 로그인한 유저의 _id 값을 호출
-      setUserInfo(response.data.item);
-    } catch (err) {
-      setError(err);
-    }
-  };
 
   // 구매 후기 등록
   const addReview = async content => {
@@ -32,15 +21,21 @@ export default function DetailFooter({_id}) {
       alert("리뷰 등록은 로그인시 이용 가능합니다.");
       navigate("/user/login"); // 로그인 페이지로 이동
       return;
+    } else if (content?.trim() === "") {
+      alert("내용을 입력해주세요");
+      return;
+    } else if (content?.length <= 1) {
+      alert("최소 2글자 이상은 입력하셔야합니다.");
+      return;
     }
     try {
       const response = await axios.post(
         `/replies`,
         {
           content: content,
-          order_id: userInfo?._id,
+          order_id: user?._id,
           product_id: product?.item?._id,
-          image: userInfo?.image,
+          image: user?.image,
         },
         {
           headers: { Authorization: `Bearer ${user?.accessToken}` }, // 로그인 상태인 유저의 엑세스  토큰
@@ -52,6 +47,7 @@ export default function DetailFooter({_id}) {
           : [response.data];
       });
       setReviewContent(""); // 리뷰 추가 후 textarea 비우기
+      fetchProduct();
     } catch (err) {
       setError(err);
     }
@@ -68,7 +64,6 @@ export default function DetailFooter({_id}) {
   };
 
   useEffect(() => {
-    fetchUser(); // 유저 정보
     fetchProduct(); // 상품 상세 정보
     setLoading(false); // 로딩 종료
   }, [_id]);
@@ -76,7 +71,6 @@ export default function DetailFooter({_id}) {
   const ProductsReviewLength = product?.item?.replies?.length; // 등록된 후기 개수
   if (loading) return <p>로딩 중...</p>;
   if (error) return <p>오류 발생: {error.message}</p>;
-  if (ProductsReviewLength === 0) return <p>리뷰가 없습니다.</p>;
   return (
     <section name="detailFooter">
       <p className="mb-10 section-title">상품 후기</p>
