b25922fc574fd76fa31b164c6264dfb7a72f7b5d
okang1014 <okang1014@gmail.com>
2025-01-09 15:17:11 +0900

    :necktie:enhancement: MainProductList, ProductCard Proptypes 지정

--- src/components/MainProductList.jsx
+++ src/components/MainProductList.jsx
@@ -1,16 +1,25 @@
 import ProductCard from "@components/common/ProductCard";
+import PropTypes from "prop-types";
+
+MainProductList.propTypes = {
+  label: PropTypes.string,
+  data: PropTypes.shape({
+    product: PropTypes.array,
+  }),
+};
 
 export default function MainProductList({ label, data }) {
-  // 최대 10개의 상품만 출력
+  // 최대 10개 상품만 출력
   let bookmarks = [];
   for (let i = 0; i < 10; i++) {
     if (!data?.product[i]) break;
     bookmarks.push(data?.product[i]);
   }
 
-  console.log(bookmarks, bookmarks.length);
-
+  // 상품의 개수만큼 margin right value 지정
   const marginRightValue = 1000 - 200 * (10 - bookmarks.length);
+
+  // ProductCard 에 props 로 전달
   const productList = bookmarks.map(item => (
     <ProductCard key={item._id} item={item} />
   ));
---
+++ src/components/common/ProductCard.jsx
@@ -0,0 +1,72 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const baseURL = "https://11.fesp.shop";
+
+ProductCard.propTypes = {
+  item: PropTypes.shape({
+    product: PropTypes.shape({
+      _id: PropTypes.number,
+      mainImages: PropTypes.array,
+      name: PropTypes.string,
+    }),
+  }),
+};
+
+export default function ProductCard({ item }) {
+  return (
+    <li className="w-[180px]">
+      {/* 상품 카드 클릭 시 상품 페이지로 이동 */}
+      <Link
+        to={`/products/${item.product._id}`}
+        aria-label="상품 페이지로 이동"
+      >
+        <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
+          <img
+            src={baseURL + item.product.mainImages[0].path}
+            alt={`${item.product.name} 상품 이미지`}
+          />
+          <button
+            type="button"
+            aria-label="상품 찜하기 버튼"
+            className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
+          >
+            <img
+              src="/assets/icons/heart-smm.svg"
+              className="w-4 h-[14px] hidden"
+            />
+            <img
+              src="/assets/icons/heart-fill-sm.svg"
+              className="w-4 h-[14px]"
+            />
+          </button>
+        </div>
+        <div className="flex items-center gap-1 mb-[10px]">
+          <p className="text-[12px] text-gray3 line-clamp-1">상품 판매자 명</p>
+          <img
+            src="/assets/icons/chevron-right.svg"
+            className="w-[3px] h-[6px]"
+          />
+        </div>
+        <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
+          {item.product.name}
+        </h3>
+        <p className="mb-[10px]">{item.product.price.toLocaleString()} 원</p>
+        <div className="flex gap-x-[6px] gap-y-1 flex-wrap">
+          <div className="px-2 py-1 w-fit text-[10px] text-white rounded-full bg-point-blue">
+            만화/애니메이션
+          </div>
+          <div className="px-2 py-1 w-fit text-[10px] text-white rounded-full bg-point-blue">
+            만화/애니메이션
+          </div>
+          <div className="px-2 py-1 w-fit text-[10px] text-white rounded-full bg-point-blue">
+            만화/애니메이션
+          </div>
+          <div className="px-2 py-1 w-fit text-[10px] text-white rounded-full bg-point-blue">
+            만화/애니메이션
+          </div>
+        </div>
+      </Link>
+    </li>
+  );
+}
