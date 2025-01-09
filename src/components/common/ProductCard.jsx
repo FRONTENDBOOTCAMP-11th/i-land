72a71e37a24312fd82ee2fa453790b84ef5a3018
okang1014 <okang1014@gmail.com>
2025-01-09 17:44:40 +0900

    :sparkles:feat: ProductCard 컴포넌트화 2 - 모든 응답데이터에 맞추어 정보 출력

--- src/components/common/ProductCard.jsx
+++ src/components/common/ProductCard.jsx
@@ -8,12 +8,10 @@ const baseURL = "https://11.fesp.shop";
 
 ProductCard.propTypes = {
   item: PropTypes.shape({
-    product: PropTypes.shape({
-      _id: PropTypes.number,
-      mainImages: PropTypes.array,
-      name: PropTypes.string,
-      price: PropTypes.number,
-    }),
+    _id: PropTypes.number,
+    mainImages: PropTypes.array,
+    name: PropTypes.string,
+    price: PropTypes.number,
   }),
 };
 
@@ -30,28 +28,25 @@ export default function ProductCard({ item }) {
   };
 
   useEffect(() => {
-    fetchSellerInfo(item.product._id);
+    fetchSellerInfo(item._id);
   }, []);
 
   return (
     <li className="w-[180px] relative">
       {/* 상품 카드 클릭 시 상품 페이지로 이동 */}
-      <Link
-        to={`/products/${item.product._id}`}
-        aria-label="상품 페이지로 이동"
-      >
+      <Link to={`/products/${item._id}`} aria-label="상품 페이지로 이동">
         <div className="aspect-[180/180] rounded-[8px] border border-gray3 overflow-hidden">
           <img
-            src={baseURL + item.product.mainImages[0].path}
-            alt={`${item.product.name} 상품 이미지`}
+            src={
+              baseURL +
+              (item.mainImages[0]?.path || "/files/final06/default-profile.png")
+            }
+            alt={`${item.name} 상품 이미지`}
           />
         </div>
       </Link>
-      <ProductLikeBtn id={item.product._id} />
-      <Link
-        to={`/products/${item.product._id}`}
-        aria-label="상품 페이지로 이동"
-      >
+      <ProductLikeBtn id={item._id} />
+      <Link to={`/products/${item._id}`} aria-label="상품 페이지로 이동">
         <div className="flex items-center gap-1 mb-[10px] pt-3">
           <p className="text-[12px] text-gray3 line-clamp-1">{sellerName}</p>
           <img
@@ -60,9 +55,9 @@ export default function ProductCard({ item }) {
           />
         </div>
         <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-          {item.product.name}
+          {item.name}
         </h3>
-        <p className="mb-[10px]">{item.product.price.toLocaleString()} 원</p>
+        <p className="mb-[10px]">{item.price.toLocaleString()} 원</p>
         <div className="flex gap-x-[6px] gap-y-1 flex-wrap">
           <div className="px-2 py-1 w-fit text-[10px] text-white rounded-full bg-point-blue">
             만화/애니메이션
