334d18f6d894ce74c8955f348410bacd254e8e5c
okang1014 <okang1014@gmail.com>
2025-01-09 11:14:58 +0900

    :sparkles:feat: 메인 페이지 로그인한 사용자의 찜한 목록 불러오기

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,21 +1,29 @@
 import MainProductList from "@components/MainProductList";
 import useAxiosInstance from "@hooks/useAxiosInstance";
 import useUserStore from "@zustand/userStore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Main() {
   // TODO 1: 로그인 상태가 아닌 경우, 찜한 상품 영역 표시 X
   const { user } = useUserStore();
 
-  console.log(user._id);
+  const [bookmark, setBookmark] = useState();
 
   const axios = useAxiosInstance();
 
   const getBookmarkedItem = async _id => {
-    const res = await axios.get(`/users/${_id}/bookmarks`);
-    console.log(res.data.item.product);
-    return res.data.item.product;
+    try {
+      const res = await axios.get(`/users/${_id}/bookmarks`);
+      const bookmarkData = res.data.item;
+      setBookmark(bookmarkData);
+    } catch (err) {
+      console.error(err.response.data.message);
+    }
   };
+
+  useEffect(() => {
+    getBookmarkedItem(user?._id);
+  }, []);
 
   return (
     <div className="container">
@@ -131,7 +139,7 @@ export default function Main() {
         </ul>
       </section>
 
-      {user && <MainProductList label="찜한 목록" />}
+      {user && <MainProductList label="찜한 목록" data={bookmark} />}
       <MainProductList label="인기 상품" />
       <MainProductList label="이번주 신상" />
       <MainProductList label="인기 판매자" />
