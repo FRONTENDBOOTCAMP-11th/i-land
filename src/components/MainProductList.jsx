2a2891a63c7a94e6dfa7bbb3706e8dabb417b863
okang1014 <okang1014@gmail.com>
2025-01-09 14:41:26 +0900

    :lipstick:style: 메인 페이지 큐레이션 영역 레이아웃 조정(완성)

--- src/components/MainProductList.jsx
+++ src/components/MainProductList.jsx
@@ -1,17 +1,17 @@
-import ProductCard from "@components/ProductCard";
+import ProductCard from "@components/common/ProductCard";
 
 export default function MainProductList({ label, data }) {
   // 최대 10개의 상품만 출력
-  let products = [];
+  let bookmarks = [];
   for (let i = 0; i < 10; i++) {
     if (!data?.product[i]) break;
-    products.push(data?.product[i]);
+    bookmarks.push(data?.product[i]);
   }
 
-  console.log(products);
+  console.log(bookmarks, bookmarks.length);
 
-  const marginRightValue = 1000 - 200 * (10 - products.length);
-  const productList = products.map(item => (
+  const marginRightValue = 1000 - 200 * (10 - bookmarks.length);
+  const productList = bookmarks.map(item => (
     <ProductCard key={item._id} item={item} />
   ));
 
@@ -20,10 +20,10 @@ export default function MainProductList({ label, data }) {
       <h2 className="section-title">{label}</h2>
       <div className="overflow-x-scroll scrollbar-hide">
         <ul
-          className={`grid 
-            ${data?.product.length < 5 ? "grid-cols-5" : `grid-cols-${data?.product.length}`}
-            gap-x-[25px] 
-            ${data?.product.length > 5 ? `mr-[-${marginRightValue}px]` : null}`}
+          className={`grid
+            grid-cols-${bookmarks.length <= 5 ? 5 : bookmarks.length}
+            gap-x-[25px]
+            mr-[-${bookmarks.length > 5 ? marginRightValue : null}px]`}
         >
           {productList}
         </ul>
