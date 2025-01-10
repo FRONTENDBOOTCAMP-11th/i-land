e30db3e42a81d1f1c282413a1d9fd5576e9da0f3
okang1014 <okang1014@gmail.com>
2025-01-10 16:52:37 +0900

    :sparkles:feat: 메인 페이지 기능 구현 완료(Issue #50 해결)
    :sparkles:feat: 메인 페이지 기능 구현현 완료(Issue #50 해결)

--- src/components/main/SellerCard.jsx
+++ src/components/main/SellerCard.jsx
@@ -1,34 +1,27 @@
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-export default function SellerCard() {
-  <li className="w-[180px]">
-    {/* 상품 카드 클릭 시 상품 페이지로 이동 */}
-    <Link to="#" aria-label="상품 페이지로 이동">
-      <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
-        <img src="" alt="" />
-        <button
-          type="button"
-          aria-label="상품 찜하기 버튼"
-          className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
-        >
-          <img
-            src="/assets/icons/heart-smm.svg"
-            className="w-4 h-[14px] hidden"
-          />
-          <img src="/assets/icons/heart-fill-sm.svg" className="w-4 h-[14px]" />
-        </button>
-      </div>
-      <div className="flex items-center gap-1 mb-[10px]">
-        <p className="text-[12px] text-gray3 line-clamp-1">Name</p>
-        <img
-          src="/assets/icons/chevron-right.svg"
-          className="w-[3px] h-[6px]"
-        />
-      </div>
-      <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-        판매자명
-      </h3>
-      <p className="mb-[10px">1,999 명</p>
-    </Link>
-  </li>;
+SellerCard.propTypes = {
+  item: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    postViews: PropTypes.number.isRequired,
+  }),
+};
+
+export default function SellerCard({ item }) {
+  return (
+    <li className="w-[180px]">
+      {/* 상품 카드 클릭 시 상품 페이지로 이동 */}
+      <Link to="#" aria-label="상품 페이지로 이동">
+        <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
+          <img src={`https://11.fesp.shop${item.image}`} alt="" />
+        </div>
+        <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
+          {item.name}
+        </h3>
+        <p className="mb-[10px">조회수 : {item.postViews.toLocaleString()}</p>
+      </Link>
+    </li>
+  );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,6 +4,7 @@ import useAxiosInstance from "@hooks/useAxiosInstance";
 import useUserStore from "@zustand/userStore";
 import { useEffect, useState } from "react";
 import CategorySection from "@components/common/CategorySection";
+import TopSellerList from "@components/main/TopSellerList";
 
 export default function Main() {
   // TODO 1: 로그인 상태가 아닌 경우, 찜한 상품 영역 표시 X
@@ -17,6 +18,9 @@ export default function Main() {
 
   // 신상품 state
   const [newProducts, setNewProducts] = useState();
+
+  // 인기 판매자 state
+  const [topSellers, setTopSellers] = useState();
 
   const axios = useAxiosInstance();
 
@@ -68,10 +72,27 @@ export default function Main() {
     }
   };
 
+  // 인기 판매자(등록한 상품 조회수 기준) 목록 조회 API
+  const getTopSellers = async () => {
+    try {
+      const res = await axios.get("/users", {
+        params: {
+          sort: JSON.stringify({ postViews: -1 }),
+          limit: 10,
+        },
+      });
+      const topSellerList = res.data.item;
+      setTopSellers(topSellerList);
+    } catch (err) {
+      console.error(err.response.data.message);
+    }
+  };
+
   useEffect(() => {
     getBookmarkedItem(user?._id);
     getTopProducts();
     getNewProducts();
+    getTopSellers();
   }, []);
 
   return (
@@ -90,303 +111,7 @@ export default function Main() {
       <MainProductList label="이번주 신상" data={newProducts} />
 
       {/* 인기 판매자 */}
-      {/* <MainProductList label="인기 판매자" data={products} /> */}
-      <section className="mb-[70px]">
-        <h2 className="section-title">인기 판매자</h2>
-        <div className="overflow-x-scroll scrollbar-hide">
-          <ul className="grid grid-cols-10 gap-x-[25px] mr-[-1000px]">
-            <li className="w-[180px]">
-              <a href="#" aria-label="상품 페이지로 이동">
-                <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
-                  <img
-                    src="/assets/images/product-image-12.png"
-                    alt="상품 제목 이미지"
-                  />
-                  <button
-                    type="button"
-                    aria-label="상품 찜하기 버튼"
-                    className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
-                  >
-                    <img
-                      src="/assets/icons/heart-sm.svg"
-                      className="w-4 h-[14px] hidden"
-                    />
-                    <img
-                      src="/assets/icons/heart-fill-sm.svg"
-                      className="w-4 h-[14px]"
-                    />
-                  </button>
-                </div>
-                <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-                  판매자 닉네임(이름)
-                </h3>
-                <p className="mb-[10px]">총 판매 횟수 1,999 건</p>
-              </a>
-            </li>
-
-            <li className="w-[180px]">
-              <a href="#" aria-label="상품 페이지로 이동">
-                <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
-                  <img
-                    src="/assets/images/product-image-12.png"
-                    alt="상품 제목 이미지"
-                  />
-                  <button
-                    type="button"
-                    aria-label="상품 찜하기 버튼"
-                    className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
-                  >
-                    <img
-                      src="/assets/icons/heart-sm.svg"
-                      className="w-4 h-[14px] hidden"
-                    />
-                    <img
-                      src="/assets/icons/heart-fill-sm.svg"
-                      className="w-4 h-[14px]"
-                    />
-                  </button>
-                </div>
-                <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-                  판매자 닉네임(이름)
-                </h3>
-                <p className="mb-[10px]">총 판매 횟수 1,999 건</p>
-              </a>
-            </li>
-
-            <li className="w-[180px]">
-              <a href="#" aria-label="상품 페이지로 이동">
-                <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
-                  <img
-                    src="/assets/images/product-image-12.png"
-                    alt="상품 제목 이미지"
-                  />
-                  <button
-                    type="button"
-                    aria-label="상품 찜하기 버튼"
-                    className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
-                  >
-                    <img
-                      src="/assets/icons/heart-sm.svg"
-                      className="w-4 h-[14px] hidden"
-                    />
-                    <img
-                      src="/assets/icons/heart-fill-sm.svg"
-                      className="w-4 h-[14px]"
-                    />
-                  </button>
-                </div>
-                <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-                  판매자 닉네임(이름)
-                </h3>
-                <p className="mb-[10px]">총 판매 횟수 1,999 건</p>
-              </a>
-            </li>
-
-            <li className="w-[180px]">
-              <a href="#" aria-label="상품 페이지로 이동">
-                <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
-                  <img
-                    src="/assets/images/product-image-12.png"
-                    alt="상품 제목 이미지"
-                  />
-                  <button
-                    type="button"
-                    aria-label="상품 찜하기 버튼"
-                    className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
-                  >
-                    <img
-                      src="/assets/icons/heart-sm.svg"
-                      className="w-4 h-[14px] hidden"
-                    />
-                    <img
-                      src="/assets/icons/heart-fill-sm.svg"
-                      className="w-4 h-[14px]"
-                    />
-                  </button>
-                </div>
-                <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-                  판매자 닉네임(이름)
-                </h3>
-                <p className="mb-[10px]">총 판매 횟수 1,999 건</p>
-              </a>
-            </li>
-
-            <li className="w-[180px]">
-              <a href="#" aria-label="상품 페이지로 이동">
-                <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
-                  <img
-                    src="/assets/images/product-image-12.png"
-                    alt="상품 제목 이미지"
-                  />
-                  <button
-                    type="button"
-                    aria-label="상품 찜하기 버튼"
-                    className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
-                  >
-                    <img
-                      src="/assets/icons/heart-sm.svg"
-                      className="w-4 h-[14px] hidden"
-                    />
-                    <img
-                      src="/assets/icons/heart-fill-sm.svg"
-                      className="w-4 h-[14px]"
-                    />
-                  </button>
-                </div>
-                <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-                  판매자 닉네임(이름)
-                </h3>
-                <p className="mb-[10px]">총 판매 횟수 1,999 건</p>
-              </a>
-            </li>
-
-            <li className="w-[180px]">
-              <a href="#" aria-label="상품 페이지로 이동">
-                <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
-                  <img
-                    src="/assets/images/product-image-12.png"
-                    alt="상품 제목 이미지"
-                  />
-                  <button
-                    type="button"
-                    aria-label="상품 찜하기 버튼"
-                    className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
-                  >
-                    <img
-                      src="/assets/icons/heart-sm.svg"
-                      className="w-4 h-[14px] hidden"
-                    />
-                    <img
-                      src="/assets/icons/heart-fill-sm.svg"
-                      className="w-4 h-[14px]"
-                    />
-                  </button>
-                </div>
-                <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-                  판매자 닉네임(이름)
-                </h3>
-                <p className="mb-[10px]">총 판매 횟수 1,999 건</p>
-              </a>
-            </li>
-
-            <li className="w-[180px]">
-              <a href="#" aria-label="상품 페이지로 이동">
-                <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
-                  <img
-                    src="/assets/images/product-image-12.png"
-                    alt="상품 제목 이미지"
-                  />
-                  <button
-                    type="button"
-                    aria-label="상품 찜하기 버튼"
-                    className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
-                  >
-                    <img
-                      src="/assets/icons/heart-sm.svg"
-                      className="w-4 h-[14px] hidden"
-                    />
-                    <img
-                      src="/assets/icons/heart-fill-sm.svg"
-                      className="w-4 h-[14px]"
-                    />
-                  </button>
-                </div>
-                <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-                  판매자 닉네임(이름)
-                </h3>
-                <p className="mb-[10px]">총 판매 횟수 1,999 건</p>
-              </a>
-            </li>
-
-            <li className="w-[180px]">
-              <a href="#" aria-label="상품 페이지로 이동">
-                <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
-                  <img
-                    src="/assets/images/product-image-12.png"
-                    alt="상품 제목 이미지"
-                  />
-                  <button
-                    type="button"
-                    aria-label="상품 찜하기 버튼"
-                    className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
-                  >
-                    <img
-                      src="/assets/icons/heart-sm.svg"
-                      className="w-4 h-[14px] hidden"
-                    />
-                    <img
-                      src="/assets/icons/heart-fill-sm.svg"
-                      className="w-4 h-[14px]"
-                    />
-                  </button>
-                </div>
-                <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-                  판매자 닉네임(이름)
-                </h3>
-                <p className="mb-[10px]">총 판매 횟수 1,999 건</p>
-              </a>
-            </li>
-
-            <li className="w-[180px]">
-              <a href="#" aria-label="상품 페이지로 이동">
-                <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
-                  <img
-                    src="/assets/images/product-image-12.png"
-                    alt="상품 제목 이미지"
-                  />
-                  <button
-                    type="button"
-                    aria-label="상품 찜하기 버튼"
-                    className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
-                  >
-                    <img
-                      src="/assets/icons/heart-sm.svg"
-                      className="w-4 h-[14px] hidden"
-                    />
-                    <img
-                      src="/assets/icons/heart-fill-sm.svg"
-                      className="w-4 h-[14px]"
-                    />
-                  </button>
-                </div>
-                <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-                  판매자 닉네임(이름)
-                </h3>
-                <p className="mb-[10px]">총 판매 횟수 1,999 건</p>
-              </a>
-            </li>
-
-            <li className="w-[180px]">
-              <a href="#" aria-label="상품 페이지로 이동">
-                <div className="relative aspect-[180/180] rounded-[8px] border border-gray3 mb-3 overflow-hidden">
-                  <img
-                    src="/assets/images/product-image-12.png"
-                    alt="상품 제목 이미지"
-                  />
-                  <button
-                    type="button"
-                    aria-label="상품 찜하기 버튼"
-                    className="absolute grid bg-white rounded-full size-7 place-items-center right-2 bottom-2"
-                  >
-                    <img
-                      src="/assets/icons/heart-sm.svg"
-                      className="w-4 h-[14px] hidden"
-                    />
-                    <img
-                      src="/assets/icons/heart-fill-sm.svg"
-                      className="w-4 h-[14px]"
-                    />
-                  </button>
-                </div>
-                <h3 className="text-[18px] font-bold mb-3 leading-normal line-clamp-2">
-                  판매자 닉네임(이름)
-                </h3>
-                <p className="mb-[10px]">총 판매 횟수 1,999 건</p>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <TopSellerList label="인기 판매자" data={topSellers} />
     </div>
   );
 }
