f02454d8060efdeefafb850db1a1c3a063a6bfe3
sunghyeon <ggjj7827@gmail.com>
2025-01-12 21:21:49 +0900

    ✨ feat: 장바구니에 상품이 없을 시 빈 장바구니 화면 출력

--- src/components/carts/CartEmpty.jsx
+++ src/components/carts/CartEmpty.jsx
@@ -1,14 +1,12 @@
 export default function cartEmpty() {
   return (
-    <body>
-      <main className="container px-24 py-5 bg-white">
-        <section name="cartHeader">
-          <div className="mt-[113px] flex flex-col items-center gap-[30px]">
-            <img src="/assets/images/error.png" alt="" />
-            <p className="text-[24px] font-bold">장바구니에 상품이 없어요 😭</p>
-          </div>
-        </section>
-      </main>
-    </body>
+    <main className="container px-24 py-5 bg-white">
+      <section name="cartHeader">
+        <div className="mt-[113px] flex flex-col items-center gap-[30px]">
+          <img src="/assets/images/error.png" alt="" />
+          <p className="text-[24px] font-bold">장바구니에 상품이 없어요 😭</p>
+        </div>
+      </section>
+    </main>
   );
 }
