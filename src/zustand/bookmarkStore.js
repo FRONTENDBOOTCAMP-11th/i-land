1237108c518eceb08a736fadf128530b8624ecbf
okang1014 <okang1014@gmail.com>
2025-01-13 13:45:06 +0900

    :sparkles:feat: 찜 전역 상태관리 store 추가

--- src/zustand/bookmarkStore.js
+++ src/zustand/bookmarkStore.js
@@ -2,21 +2,21 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 // import { cookieStorage } from "zustand-cookie-storage";
 
-const LikeStore = set => ({
+const BookmarkStore = set => ({
   // 찜 기본값 false
-  like: false,
+  bookmark: null,
   // 찜 상품의 상태값 지정, 서버로부터 받아온 찜한 정보 표시
-  setLike: like => set({ like }),
+  setBookmark: bookmark => set({ bookmark }),
   // 찜 취소
-  resetLike: () => set({ like: null }),
+  resetBookmark: () => set({ bookmark: null }),
 });
 
 // sessionStorage 에 찜한 정보 저장
-const useLikeStore = create(
-  persist(LikeStore, {
-    name: "like",
+const useBookmarkStore = create(
+  persist(BookmarkStore, {
+    name: "bookmark",
     storage: createJSONStorage(() => sessionStorage),
   }),
 );
 
-export default useLikeStore;
+export default useBookmarkStore;
