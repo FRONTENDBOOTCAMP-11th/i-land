59e5fbcf45107586f1415adb7a9467a7d8d6f983
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2025-01-13 16:48:18 +0900

    ✨feat: 카테고리별 상품 리스트 페이지 기능 구현 1차 완료

--- src/components/common/CategorySection.jsx
+++ src/components/common/CategorySection.jsx
@@ -1,24 +1,19 @@
+import PropTypes from "prop-types";
 import { Link, useLocation } from "react-router-dom";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 import useAxiosInstance from "@hooks/useAxiosInstance";
 
 import CategoryCard from "@components/common/CategoryCard";
 
-// URL : https://11.fesp.shop/products?custom={"extra.category": "카테고리"}
-
-export default function CategorySection() {
+export default function CategorySection({ onCategorySelect }) {
   const location = useLocation();
-  const queryClient = useQueryClient();
+
   const axios = useAxiosInstance();
   const isMainPage = location.pathname === "/";
 
   // 카테고리 데이터 서버에서 불러오기
-  const {
-    data: categories,
-    isLoading,
-    error,
-  } = useQuery({
+  const { data: categories = [] } = useQuery({
     queryKey: ["productCategory"],
     queryFn: async () => {
       const response = await axios.get("/codes/productCategory");
@@ -29,21 +24,10 @@ export default function CategorySection() {
     },
   });
 
-  const handleCategoryClick = async categoryName => {
-    const { queryKey, url } = getCategoryData(categoryName);
-
-    // 캐시 데이터 확인
-    const cachedData = queryClient.getQueryData(queryKey);
-    if (cachedData) {
-      console.log("Using cached data: ", cachedData);
-      return;
-    }
-
-    try {
-      // 서버에서 데이터 가져오기
-      await axios.get(url);
-    } catch (error) {
-      console.log("Error fetching category data : ", error);
+  const handleCategoryClick = categoryName => {
+    // 부모 컴포넌트로 선택된 카테고리 전달
+    if (onCategorySelect) {
+      onCategorySelect(categoryName);
     }
   };
 
@@ -51,41 +35,35 @@ export default function CategorySection() {
     <section className="mb-[70px]">
       {isMainPage && <h2 className="section-title">카테고리</h2>}
       <ul className="grid grid-flow-col grid-cols-6 gap-8">
-        {isLoading ? (
-          <p>Loading categories...</p>
-        ) : error ? (
-          <p>Error loading categories</p>
-        ) : (
-          categories.map(category =>
-            isMainPage ? (
-              <Link
-                key={category.code}
-                to={`/products?extra={"extra.category":"${category.code}"}`}
-              >
-                <CategoryCard
-                  categoryIconSrc={category.path}
-                  categoryName={category.code}
-                  categoryText={category.value}
-                />
-              </Link>
-            ) : (
+        {categories.map(category =>
+          isMainPage ? (
+            <Link
+              key={category.code}
+              to={`/products?custom=${encodeURIComponent(
+                JSON.stringify({ "extra.category": category.code }),
+              )}`}
+            >
               <CategoryCard
-                key={category.code}
                 categoryIconSrc={category.path}
                 categoryName={category.code}
                 categoryText={category.value}
-                onClick={() => handleCategoryClick(category.code)}
               />
-            ),
-          )
+            </Link>
+          ) : (
+            <CategoryCard
+              key={category.code}
+              categoryIconSrc={category.path}
+              categoryName={category.code}
+              categoryText={category.value}
+              onClick={() => handleCategoryClick(category.code)}
+            />
+          ),
         )}
       </ul>
     </section>
   );
 }
 
-function getCategoryData(categoryName) {
-  const queryKey = ["products", categoryName];
-  const url = `/products?custom=${JSON.stringify({ "extra.category": categoryName })}`;
-  return { queryKey, url };
-}
+CategorySection.propTypes = {
+  onCategorySelect: PropTypes.func,
+};
