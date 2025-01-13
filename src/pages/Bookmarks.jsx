e29eeedb9a4046a07674e3e0fc8b1a7d3daf6537
sunghyeon <ggjj7827@gmail.com>
2025-01-13 16:19:55 +0900

    ✨ feat: 찜한 상품 페이지에 찜한 상품이 0개일시 빈 찜한상품 페이지 출력

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import useAxiosInstance from "@hooks/useAxiosInstance";
+import BookmarksEmpty from "@components/bookmarks/bookmarksEmpty";
 
 export default function Bookmarks() {
   const axios = useAxiosInstance();
@@ -92,66 +93,72 @@ export default function Bookmarks() {
       </section>
 
       <ul className="grid grid-flow-row gap-y-[50px] pb-[60px]">
-        {bookmarks?.item?.map(bookmarkslist => {
-          // 각 cartlist에 대한 productItem 찾기
-          const productItem = product?.item?.find(
-            prod => prod._id === bookmarkslist.product._id,
-          );
-          // 판매자 정보 가져오기
-          const sellerName = productItem?.seller?.name;
-          return (
-            <li
-              key={bookmarkslist.product._id}
-              className="p-10 flex justify-between gap-x-[60px] border border-gray2 rounded-lg box-border items-center"
-            >
-              <div className="flex gap-x-[60px]">
-                <Link to={`/products/${productItem?._id}`}>
-                  <img
-                    src={`https://11.fesp.shop//${bookmarkslist?.product?.mainImages[0]?.path}`}
-                    className="size-[150px]"
-                    alt="상품 대표 이미지"
-                  />
-                </Link>
-                <div className="grid grid-flow-row gap-y-[14px] items-center">
-                  <div className="text-[18px] text-gray3 flex gap-x-[10px] items-center">
-                    {sellerName}
-                    <img
-                      src="/assets/icons/chevron-right.svg"
-                      className="w-[6px] h-3"
-                    />
+        {bookmarks.item?.length === 0 ? (
+          <BookmarksEmpty />
+        ) : (
+          <>
+            {bookmarks?.item?.map(bookmarkslist => {
+              // 각 cartlist에 대한 productItem 찾기
+              const productItem = product?.item?.find(
+                prod => prod._id === bookmarkslist.product._id,
+              );
+              // 판매자 정보 가져오기
+              const sellerName = productItem?.seller?.name;
+              return (
+                <li
+                  key={bookmarkslist.product._id}
+                  className="p-10 flex justify-between gap-x-[60px] border border-gray2 rounded-lg box-border items-center"
+                >
+                  <div className="flex gap-x-[60px]">
+                    <Link to={`/products/${productItem?._id}`}>
+                      <img
+                        src={`https://11.fesp.shop//${bookmarkslist?.product?.mainImages[0]?.path}`}
+                        className="size-[150px]"
+                        alt="상품 대표 이미지"
+                      />
+                    </Link>
+                    <div className="grid grid-flow-row gap-y-[14px] items-center">
+                      <div className="text-[18px] text-gray3 flex gap-x-[10px] items-center">
+                        {sellerName}
+                        <img
+                          src="/assets/icons/chevron-right.svg"
+                          className="w-[6px] h-3"
+                        />
+                      </div>
+                      <h2 className="text-[32px] font-bold">
+                        {bookmarkslist?.product?.name}
+                      </h2>
+                      <p className="text-[18px]">
+                        <span className="text-[24px] font-bold">
+                          {bookmarkslist?.product?.price.toLocaleString()}
+                        </span>
+                        원
+                      </p>
+                    </div>
                   </div>
-                  <h2 className="text-[32px] font-bold">
-                    {bookmarkslist?.product?.name}
-                  </h2>
-                  <p className="text-[18px]">
-                    <span className="text-[24px] font-bold">
-                      {bookmarkslist?.product?.price.toLocaleString()}
-                    </span>
-                    원
-                  </p>
-                </div>
-              </div>
-              <div className="flex gap-x-[10px]">
-                <button
-                  type="button"
-                  className="w-[150px] h-[50px] py-[14px] px-9 rounded-lg text-[18px] font-bold border-2 border-gray2 box-border"
-                  aria-label="장바구니 담기 버튼"
-                  onClick={() => addCart(bookmarkslist.product._id)}
-                >
-                  장바구니
-                </button>
-                <button
-                  type="button"
-                  className="w-[120px] h-[50px] py-[14px] px-9 rounded-lg text-[18px] font-bold text-white bg-point-red box-border"
-                  aria-label="찜하기 목록에서 삭제 버튼"
-                  onClick={() => DeleteBookmarks(bookmarkslist._id)}
-                >
-                  삭제
-                </button>
-              </div>
-            </li>
-          );
-        })}
+                  <div className="flex gap-x-[10px]">
+                    <button
+                      type="button"
+                      className="w-[150px] h-[50px] py-[14px] px-9 rounded-lg text-[18px] font-bold border-2 border-gray2 box-border"
+                      aria-label="장바구니 담기 버튼"
+                      onClick={() => addCart(bookmarkslist.product._id)}
+                    >
+                      장바구니
+                    </button>
+                    <button
+                      type="button"
+                      className="w-[120px] h-[50px] py-[14px] px-9 rounded-lg text-[18px] font-bold text-white bg-point-red box-border"
+                      aria-label="찜하기 목록에서 삭제 버튼"
+                      onClick={() => DeleteBookmarks(bookmarkslist._id)}
+                    >
+                      삭제
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
+          </>
+        )}
       </ul>
     </div>
   );
