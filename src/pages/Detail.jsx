f990f449a159f4dd810e9db3eb85994f44107628
okang1014 <okang1014@gmail.com>
2025-01-14 16:58:35 +0900

    :necktie:enhancement: useParams 로 받아오는 상품 id 숫자 타입으로 변경

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -6,11 +6,12 @@ import useUserStore from "@zustand/userStore";
 export default function Detail() {
   const { user } = useUserStore();
   const { _id } = useParams(); // URL에서 id 추출
+  const product_id = Number(_id);
   return (
     <main className="container px-24 py-5 bg-white">
-      <DetailHeader _id={_id} user={user} />
+      <DetailHeader _id={product_id} user={user} />
       <hr className="text-gray1 border border-solid my-10"></hr>
-      <DetailFooter _id={_id} user={user} />
+      <DetailFooter _id={product_id} user={user} />
     </main>
   );
 }
