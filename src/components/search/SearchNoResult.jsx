273fb9052cb6b28a3c449a371b368b0e41a18790
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2025-01-14 15:07:06 +0900

    ✨ feat: 검색 결과 없음 페이지 기능 구현

--- src/components/search/SearchNoResult.jsx
+++ src/components/search/SearchNoResult.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function SearchNoResult() {
   return (
     <div className="mt-[110px] flex flex-col items-center gap-[30px]">
       <img src="/src/assets/images/error.png" alt="검색 결과 없음" />
       <p className="text-[24px] font-bold">검색 결과가 없어요 😭</p>
+      <Link to="/" className="text-blue-500 underline">
+        홈으로 돌아가기
+      </Link>
     </div>
   );
 }
