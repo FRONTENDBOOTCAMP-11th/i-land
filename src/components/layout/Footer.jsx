77685a268dcc83e04b2aba8103ac142b20637b20
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2025-01-16 11:33:25 +0900

    ✨feat: Footer img 태그 내 속성 위치 변경

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -32,29 +32,29 @@ export default function Footer() {
           <a href="https://www.instagram.com">
             <img
               src="/assets/icons/instagram.svg"
-              alt="Instagram"
               className="w-6 h-6"
+              alt="Instagram"
             />
           </a>
           <a href="https://www.facebook.com">
             <img
               src="/assets/icons/facebook.svg"
-              alt="Facebook"
               className="w-6 h-6"
+              alt="Facebook"
             />
           </a>
           <a href="https://www.twitter.com">
             <img
               src="/assets/icons/twitter.svg"
-              alt="Twitter"
               className="w-6 h-6"
+              alt="Twitter"
             />
           </a>
           <a href="https://www.kakaocorp.com">
             <img
               src="/assets/icons/kakaotalk.svg"
-              alt="KakaoTalk"
               className="w-6 h-6"
+              alt="KakaoTalk"
             />
           </a>
         </div>
