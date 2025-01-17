61cfb51ccae3e95c014e6b17bbbb9d0bf05d3bd3
sunghyeon <ggjj7827@gmail.com>
2025-01-17 13:06:27 +0900

    💄 style: 버튼 간격 설정

--- src/components/common/MoveScroll.jsx
+++ src/components/common/MoveScroll.jsx
@@ -3,13 +3,12 @@ import { Link } from 'react-scroll';
 
 export default function MoveScroll() {
   return(
-    <nav className="flex flex-col text-center">
+    <nav className="flex flex-col text-center gap-[3px]">
       <Link to="header" smooth={true} duration={500} className="">
         <button className="text-gray3 w-[80px] h-[40px] border border-gray2 bg-white hover:bg-point-blue hover:border-none hover:text-white rounded-t-xl">
           TOP
         </button>
       </Link>
-        <hr className="text-white border-t-[3px]"></hr>
       <Link to="footer" smooth={true} duration={500}>
         <button className="text-gray3 w-[80px] h-[40px] border border-gray2 bg-white hover:bg-point-blue hover:border-none hover:text-white rounded-b-xl">
           BOTTOM
