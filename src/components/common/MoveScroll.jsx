3e24b35233e83f222971f9185abc234f4088d510
sunghyeon <ggjj7827@gmail.com>
2025-01-17 13:13:15 +0900

    ♻️refactor: 불필요한 태그 속성 삭제

--- src/components/common/MoveScroll.jsx
+++ src/components/common/MoveScroll.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-scroll';
 export default function MoveScroll() {
   return(
     <nav className="flex flex-col text-center gap-[3px]">
-      <Link to="header" smooth={true} duration={500} className="">
+      <Link to="header" smooth={true} duration={500}>
         <button className="text-gray3 w-[80px] h-[40px] border border-gray2 bg-white hover:bg-point-blue hover:border-none hover:text-white rounded-t-xl">
           TOP
         </button>
