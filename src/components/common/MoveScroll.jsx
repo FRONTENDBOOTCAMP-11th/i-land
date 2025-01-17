8bee40b63fcbc64efc55acc5e51b22f1951edc1c
sunghyeon <ggjj7827@gmail.com>
2025-01-17 13:05:20 +0900

    💄 style: 버튼 디자인 수정

--- src/components/common/MoveScroll.jsx
+++ src/components/common/MoveScroll.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-scroll';
 
 export default function MoveScroll() {
   return(
-    <nav className="flex flex-col text-white text-center">
+    <nav className="flex flex-col text-center">
       <Link to="header" smooth={true} duration={500} className="">
-        <button className="w-[80px] h-[30px] bg-point-blue hover:bg-point-red rounded-t-3xl">
+        <button className="text-gray3 w-[80px] h-[40px] border border-gray2 bg-white hover:bg-point-blue hover:border-none hover:text-white rounded-t-xl">
           TOP
         </button>
       </Link>
-        <hr className="border-t-[3px]"></hr>
+        <hr className="text-white border-t-[3px]"></hr>
       <Link to="footer" smooth={true} duration={500}>
-        <button className="w-[80px] h-[30px] bg-point-blue hover:bg-point-red rounded-b-3xl">
+        <button className="text-gray3 w-[80px] h-[40px] border border-gray2 bg-white hover:bg-point-blue hover:border-none hover:text-white rounded-b-xl">
           BOTTOM
         </button>
       </Link>
