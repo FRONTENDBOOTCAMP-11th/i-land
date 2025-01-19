3fdc317c9fa579e5c5266750825315cf01e65684
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2025-01-19 14:29:56 +0900

    ✨feat: 상품 상세페이지 Spinner 효과 추가

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,13 +1,15 @@
-import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import useUserStore from "@zustand/userStore";
+import { useNavigate, useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+
 import useAxiosInstance from "@hooks/useAxiosInstance";
+import useUserStore from "@zustand/userStore";
+
 import ProductsDetailInfomation from "@components/detail/ProductsDetailInfomation";
 import ProductsExplanation from "@components/detail/ProductsExplanation";
 import AddReview from "@components/detail/AddReview";
 import ReviewList from "@components/detail/ReviewList";
-import { Helmet } from "react-helmet-async";
+import useLoadingStore from "@zustand/useLoadingStore";
 
 export default function Detail() {
   const axios = useAxiosInstance();
@@ -15,21 +17,29 @@ export default function Detail() {
   const { user } = useUserStore();
   const { _id } = useParams(); // URL에서 id 추출
   const products_id = Number(_id);
-  const [loading, setLoading] = useState(true); // 로딩
+
+  const startLoading = useLoadingStore(state => state.startLoading);
+  const stopLoading = useLoadingStore(state => state.stopLoading);
+
   const [error, setError] = useState(null); // 에러
+
   const [products, setProduct] = useState(null); // 상품 초기값 null
   const [like, setLike] = useState(null); // 찜 상태
   const [reviewContent, setReviewContent] = useState(""); // textarea 상태
+
   const ProductsReviewLength = products?.item?.replies?.length; // 등록된 후기 개수
   const ProductsReview = products?.item?.replies; //  등록된 후기 (in products)
 
   // 상품 상세 조회 (/products/{_id})
   const fetchProduct = async () => {
+    startLoading();
     try {
       const response = await axios.get(`/products/${_id}`);
       setProduct(response?.data);
-    } catch (err) {
-      setError(err);
+    } catch (error) {
+      console.error(error.response?.data?.message);
+    } finally {
+      stopLoading();
     }
   };
 
@@ -52,6 +62,7 @@ export default function Detail() {
       alert("최소 2글자 이상은 입력하셔야합니다.");
       return;
     }
+    startLoading();
     try {
       await axios.post(`/replies`, {
         content: content,
@@ -61,12 +72,16 @@ export default function Detail() {
       });
       setReviewContent(""); // 리뷰 추가 후 textarea 비우기
       fetchProduct();
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      console.error(error.response?.data?.message);
+    } finally {
+      stopLoading();
     }
   };
+
   // 찜 상태 확인
   const checkIfLiked = async () => {
+    startLoading();
     try {
       const response = await axios.get(`/bookmarks/product/${products_id}`);
       if (response.data && response.data.item) {
@@ -74,12 +89,15 @@ export default function Detail() {
       } else {
         setLike(null);
       }
-    } catch (err) {
-      if (err.response.status !== 404) {
-        console.error(err.response.data.message);
+    } catch (error) {
+      if (error.response.status !== 404) {
+        console.error(error.response.data.message);
       }
+    } finally {
+      stopLoading();
     }
   };
+
   // _id값 변경시 실행
   useEffect(() => {
     // 로그인 상태가 아니라면 찜하기 상태 불러오지 않음
@@ -87,13 +105,12 @@ export default function Detail() {
       checkIfLiked();
     }
     fetchProduct(); // 상품 정보 가져오기
-    setLoading(false); // 로딩 종료
   }, [_id]);
 
   // 정상 작동이 안 될 시에 로딩, 에러 표시
-  if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
   if (!products) return <div>상품 정보를 불러오는 중입니다...</div>;
+
   return (
     <>
       <Helmet>
