faa79743c5c30bccf61d5a075e04ca12f79533d7
sunghyeon <ggjj7827@gmail.com>
2025-01-20 15:13:12 +0900

    ♻️refactor: 결제 요청 코드 수정

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -36,53 +36,43 @@ export default function Payment() {
   const totalPrice = buyProduct?.price * quantitycount;
 
   const requestPay = async () => {
-    if (window.PortOne) {
-      // 결제 요청
-      window.PortOne.requestPayment(
-        {
-          storeId: "store-e4038486-8d83-41a5-acf1-844a009e0d94",
-          paymentId: "a" + new Date().getTime(),
-          orderName: buyProduct?.name,
-          totalAmount: totalPrice,
-          currency: "KRW",
-          channelKey: "channel-key-ebe7daa6-4fe4-41bd-b17d-3495264399b5",
-          payMethod: "CARD",
-          customer: {
-            customerId: customer?.email,
-            fullName: customer?.name,
-            phoneNumber: customer?.phone,
-            email: customer?.email,
-          },
-          windowType: {
-            pc: "IFRAME",
-          },
-        },
-        async response => {
-          // 결제 성공 후 주문 생성
-          if (response.status === "success") {
-            try {
-              await axios.post(`/orders/`, {
-                products: [
-                  {
-                    _id: buyProduct._id,
-                    quantity: quantitycount,
-                  },
-                ],
-              });
-              alert("구매가 완료되었습니다.");
-            } catch (error) {
-              console.error("주문 생성 실패:", error);
-            }
-          } else {
-            alert("결제에 실패하였습니다. 다시 시도해 주세요.");
-          }
-        },
-      );
+    const response = await window.PortOne.requestPayment({
+      storeId: "store-e4038486-8d83-41a5-acf1-844a009e0d94",
+      paymentId: "a" + new Date().getTime(),
+      orderName: buyProduct?.name,
+      totalAmount: totalPrice,
+      currency: "KRW",
+      channelKey: "channel-key-ebe7daa6-4fe4-41bd-b17d-3495264399b5",
+      payMethod: "CARD",
+      customer: {
+        customerId: customer?.email,
+        fullName: customer?.name,
+        phoneNumber: customer?.phone,
+        email: customer?.email,
+      },
+      windowType: {
+        pc: "IFRAME",
+      },
+    });
+    // 결제 성공 후 주문 생성
+    if (response.status === "success") {
+      try {
+        await axios.post(`/orders/`, {
+          products: [
+            {
+              _id: buyProduct._id,
+              quantity: quantitycount,
+            },
+          ],
+        });
+        alert("구매가 완료되었습니다.");
+      } catch (error) {
+        console.error("주문 생성 실패:", error);
+      }
     } else {
-      console.error("PortOne SDK is not loaded");
+      alert("결제에 실패하였습니다. 다시 시도해 주세요.");
     }
   };
-
   useEffect(() => {
     // 외부 스크립트 로드
     const script = document.createElement("script");
@@ -112,22 +102,28 @@ export default function Payment() {
         />
       </Helmet>
       <div className="container">
-        <div className="flex">
-          <img src="" alt="결제할 상품 이미지" />
-          <div className="flex flex-col">
-            <p>{buyProduct?.name}</p>
-            <div className="flex justify-between">
-              <p>{quantitycount}</p> <p>{buyProduct?.price}</p>
+        <div className="flex flex-col items-center gap-y-[20px]">
+          <div className="flex border border-gray1-[2px] rounded-lg p-[20px] gap-[20px] items-center">
+            <img
+              className="w-[100px] h-[100px]"
+              src={"https://11.fesp.shop" + buyProduct?.mainImages[0]?.path}
+              alt="결제할 상품 이미지"
+            />
+            <div className="flex flex-col gap-y-[10px]">
+              <p>{buyProduct?.name}</p>
+              <p className="flex justify-end">{buyProduct?.price} 원</p>
+              <div className="flex justify-between">
+                <p>{quantitycount} 개</p> <p>총 {totalPrice} 원</p>
+              </div>
             </div>
-            <p>{totalPrice}</p>
           </div>
+          <button
+            className="w-[400px] h-[60px] mb-[60px] mt-[60px] px-[89px] py-[16px] bg-point-blue text-white rounded-[8px] text-[24px] font-bold"
+            onClick={requestPay}
+          >
+            결제하기
+          </button>
         </div>
-        <button
-          className="w-[400px] h-[60px] mb-[60px] mt-[60px] px-[89px] py-[16px] bg-point-blue text-white rounded-[8px] text-[24px] font-bold"
-          onClick={requestPay}
-        >
-          결제하기
-        </button>
       </div>
     </>
   );
