60fbab9a5a9d068fa87bbd864ae2530f071a1d2d
okang1014 <okang1014@gmail.com>
2025-01-21 11:36:13 +0900

    :lipstick:style: 상품 상세 페이지 정방형이 아닌 상품 이미지 대웅

--- src/components/detail/ProductsDetailInfomation.jsx
+++ src/components/detail/ProductsDetailInfomation.jsx
@@ -134,7 +134,7 @@ export default function ProductsDetailInfomation({
         <div className="flex items-center gap-x-20">
           <div className="relative w-[480px] h-[480px]">
             <img
-              className="w-full h-full"
+              className="w-full h-full object-contain"
               src={`https://11.fesp.shop${mainImages[imgcount]?.path || "/files/final06/default-profile.png"}`}
               alt="상품 이미지"
             />
